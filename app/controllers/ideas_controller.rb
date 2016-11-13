b4c782cb9f16f3309fba37df756a62a9c2f89d38
John Doe <chiachen1225@gmail.com>
2016-11-13 15:36:27 -0800

    add ideas crud

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,9 +1,9 @@
 class IdeasController < ApplicationController
 
+  before_action  :find_idea, {only: [:show, :edit, :update, :destroy]}
+
   def index
     @ideas = Idea.all.order(created_at: :desc)
-
-
   end
 
   def new
@@ -11,9 +11,18 @@ class IdeasController < ApplicationController
   end
 
   def create
+    @idea = Idea.new idea_params
+    if @idea.save
+      redirect_to idea_path(@idea), notice: "New Idea Created"
+    else
+      flash[:alert] = "noooo idea"
+      render :new
+    end
   end
 
   def show
+
+
   end
 
   def edit
@@ -21,12 +30,29 @@ class IdeasController < ApplicationController
   end
 
   def update
+    if @idea.update idea_params
+      redirect_to idea_path(@idea)
+    else
+      flash[:alert] = "fix errors"
+      render :edit
+    end
   end
 
   def destroy
+    @idea.destroy
+    redirect_to root_path
+    
   end
 
   private
+
+  def idea_params
+    params.require(:idea).permit([:title, :body])
+  end
+
+  def find_idea
+    @idea = Idea.find params[:id]
+  end
 
 
 end
